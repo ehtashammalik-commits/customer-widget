be3039a15a626d64efcf9d6ccfc8b1e45194cfe8
Ahsan Raza <ahsan.raza@expertflow.com>
2023-09-08 11:41:34 +0000

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "loading"
 #envsubst '${DB_URL},${}'< ormconfig.json > ormconfig.json
-envsubst < config.json.template > /usr/share/nginx/html/assets/config/config.json
-envsubst < config.js.template > assets/widget/config.js
-echo "Printing config.json"
-cat assets/config/config.json
-echo "=========================="
-echo "Starting Customer Gadget"
+# envsubst < config.json.template > assets/config/config.json
+# envsubst < config.js.template > assets/widget/config.js
+# echo "Printing config.json"
+# cat assets/config/config.json
+# echo "=========================="
+# echo "Starting Customer Gadget"
 
 exec nginx -g "daemon off;" "$@"
 
