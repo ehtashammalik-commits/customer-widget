ee524902c4c1a39c54e7cf429e095dc7136ad555
Mobeen Shah <41565155+mobeen887@users.noreply.github.com>
2023-09-08 15:12:20 +0500

    updated docker entry point path

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "loading"
 #envsubst '${DB_URL},${}'< ormconfig.json > ormconfig.json
-envsubst < config.json.template > assets/config/config.json
+envsubst < config.json.template > /usr/share/nginx/html/assets/config/config.json
 envsubst < config.js.template > assets/widget/config.js
 echo "Printing config.json"
 cat assets/config/config.json
